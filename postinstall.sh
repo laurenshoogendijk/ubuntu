d4b2c2d32e3838bf0ada4a19a11c22ba258fdd7d
laurens <laurens@shockmedia.nl>
2024-07-16 16:22:06 +0200

    Add hostname to postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-# Settings
+# Ask for then set hostname
 read -p "Hostname: " hostname
+hostnameshort=$(echo $hostname | cut -f1 -d.)
+
+hostnamectl set-hostname $hostname
+
+echo "127.0.0.1 localhost
+127.0.1.1 $hostname $hostnameshort
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters" > /etc/hosts
+
+# Auto install packages
+/bin/bash packages_install.sh
